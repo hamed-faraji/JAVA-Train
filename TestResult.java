12ce0807b02579e605f1538b3da74a6aa0c5790c
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-07-28 10:35:00 +0430

    add main

---
+++ TestResult.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TestResult {
+	static void process(String answer, String data) {}
+
+	
+	public static void main(String args[]) {/// do not edit main method, input
+										/// should be entered as described
+										/// before
+	String answer, data;
+	scanner2 = new Scanner(System.in);
+	answer = scanner2.nextLine();/// first line of input is the correct
+								/// answer
+	data = scanner2.nextLine();/// second line of input is data as described
+								/// before
+	process(answer, data);
+}
+}
