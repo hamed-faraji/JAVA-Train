64aae383d69b82ec6a96031f2f5432454a4ff548
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-08 21:35:26 +0430

    add scanner and split some data

---
+++ StringFunctions.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringFunctions {
+	private static Scanner scanner;
+
+	public static void main(String args[]) {
+		
+		scanner = new Scanner(System.in);
+		String line;
+		line = scanner.nextLine();/// first line of input.
+		String command;
+		String temp1;
+		// jodasazie dastoorha:
+		String[] parts = line.split("_");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
