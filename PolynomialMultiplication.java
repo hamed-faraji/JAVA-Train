4b4a29648ba17141745e9773e73947636a8f5a9a
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-23 22:13:54 +0430

    power continues

--- PolynomialMultiplication.java
+++ PolynomialMultiplication.java
@@ -39,6 +39,31 @@ public class PolynomialMultiplication {
 			if (term1[i].contains("x")) {
 				if (term1[i].equals("+1x")) {
 					int a = 1;
+					// special ifs for exeptions:
+							if (term2[j].equals("+1x")) {
+								int c = 1, e = a * c;
+								allList.add(e + "x" + "^" + 2);
+								} else if (term2[j].equals("-1x")) {
+								int c = -1, e = a * c;
+								allList.add(e + "x" + "^" + 2);
+								} else {
+								String[] temp2 = term2[j].split("x");
+								int c = Integer.parseInt(temp2[0]);
+								if (term2[j].contains("^")) {
+									temp2[1] = temp2[1].replace("^", "");
+									int d = Integer.parseInt(temp2[1]), e = a * c, f = d + 1;
+									allList.add(e + "x" + "^" + f);
+									} else {//// ghesmate marboot be x ha: (baraye term2)
+									int e = a * c;
+									allList.add(e + "x" + "^" + 2);
+									}
+							}
+						} else {
+							int c = Integer.parseInt(term2[j]), e = a * c;
+							allList.add(e + "x");
+						}
+					}
+					// ghesmate marboot be x ha:(baraye term1)
 					
 				} else if (term1[i].equals("-1x")) {
 					
