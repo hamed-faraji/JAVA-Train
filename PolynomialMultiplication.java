43880b95c7d348d070a0ade07f4b59eb2f1ae815
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-23 22:06:35 +0430

    Add some preprocessing to calculate polynomial

--- PolynomialMultiplication.java
+++ PolynomialMultiplication.java
@@ -6,6 +6,17 @@ public class PolynomialMultiplication {
 	private static Scanner scanner2;
 
 	static void process(String polynomial1, String polynamial2) {
+		String[] term1 = polynomial1.split(" ");
+		String[] term2 = polynamial2.split(" ");
+		// if first term be x change it to +x:
+		if (term1[0].contains("x")) {
+			if (term1[0].charAt(0) == 'x')
+				term1[0] = String.join("", "+", term1[0]);
+		}
+		if (term2[0].contains("x")) {
+			if (term2[0].charAt(0) == 'x')
+				term2[0] = String.join("", "+", term2[0]);
+		}
 		
 		
 		}
