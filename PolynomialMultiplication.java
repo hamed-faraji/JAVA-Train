b75917da8127a32ee328a9ae2348c0346c681c54
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-23 22:05:11 +0430

    Add main and scanner

--- PolynomialMultiplication.java
+++ PolynomialMultiplication.java
@@ -1,1 +1,25 @@
-lkdm
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PolynomialMultiplication {
+
+	private static Scanner scanner2;
+
+	static void process(String polynomial1, String polynamial2) {
+		
+		
+		}
+
+	public static void main(String args[]) {
+
+		String polynomial1, polynomial2;
+		scanner2 = new Scanner(System.in);
+		polynomial1 = scanner2.nextLine();
+
+		polynomial2 = scanner2.nextLine();
+
+		process(polynomial1, polynomial2);
+
+	}
+
+}
